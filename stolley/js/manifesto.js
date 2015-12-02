9680d2be88c0bd6f0dfaa4e7aa951805d4458873
Karl Stolley <karl.stolley@gmail.com>
2015-12-02 16:03:33 -0600

    Responsively-loading ASCII art. Need to rework <figure> to be accessible

--- stolley/js/manifesto.js
+++ stolley/js/manifesto.js
@@ -1,6 +1,7 @@
 jQuery.noConflict();
 (function($) {
   $(function() {
+    $('html').removeClass('nojs').addClass('js');
     $('abbr[title]').each(function() {
       var abbr, expandedText;
       abbr = $(this).text();
@@ -10,6 +11,19 @@ jQuery.noConflict();
     $('abbr[title]').on('click keypress', function() {
       $(this).html($(this).attr('data-expanded')).addClass('expanded').removeAttr('title tabindex');
     });
+    // Grabbing breakpoints from the CSS media queries using a technique outlined at
+    //https://www.lullabot.com/articles/importing-css-breakpoints-into-javascript
+    var breakpoint = {};
+    breakpoint.refreshValue = function () {
+      this.value = window.getComputedStyle(document.querySelector('body'), ':after').getPropertyValue('content').replace(/\"/g, '');
+    };
+    $(window).resize(function () {
+      breakpoint.refreshValue();
+      loadASCIIArt();
+    }).resize();
+    function loadASCIIArt() {
+      $('#ascii').html($('#ascii-' + breakpoint.value).html());
+    }
   });
 })(jQuery);
 
